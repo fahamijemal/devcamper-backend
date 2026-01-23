50372d0ab6e907f9f418acb33e6a10b505ac0ce1
Fahami Jemal <fahamijemal1@gmail.com>
2026-01-24 00:27:46 +0300

    implemented db connection and created Bootcamp model

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,6 @@
+//@esc logs request to console
+
+
 const logger = (req, res, next) => {
     console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
     next();
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const colors = require('colors');
 const bootcamps = require('./routes/bootcamps'); //route files
 const logger = require('./middleware/logger');
 dotenv.config({path: './config/config.env'}); //load env vars
 
+
+
+//connect to database
+connectDB();
 const app =express();
+
+
 app.use(logger); //using logger middleware
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
