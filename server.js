0a4068b4646d9717e5313039e4db1e2270f621c0
Fahami Jemal <fahamijemal1@gmail.com>
2026-01-25 16:45:42 +0300

    replaced custom loger with  morgan

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const colors = require('colors');
+const morgan = require('morgan');
 const bootcamps = require('./routes/bootcamps'); //route files
-const logger = require('./middleware/logger');
-dotenv.config({path: './config/config.env'}); //load env vars
+dotenv.config({ path: './config/config.env' }); //load env vars
 
 
+//body parser
+//create express app
+const app = express();
+
+//body parser
+app.use(express.json());
 
 //connect to database
 connectDB();
-const app =express();
 
-
-app.use(logger); //using logger middleware
-//mount routers
+// morgan middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
